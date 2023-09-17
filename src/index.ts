86ccf27eb7afcff7cb9439ab2e9b5a9fc55ea7d7
RyanOlder <68904054+RyanOld@users.noreply.github.com>
2023-09-17 07:47:31 +0700

    prettier-eslint setup running

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('test')
+import Hapi from "@hapi/hapi";
+
+const init = async (): Promise<void> => {
+  const server = Hapi.server({
+    port: 5000,
+    host: "localhost",
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+void init();
