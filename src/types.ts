002f36b9e6486305b2c188aa8a4036790adacb7f
RyanOlder <68904054+RyanOld@users.noreply.github.com>
2023-09-17 10:02:33 +0700

    add type for books in storage(json)

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
-export interface Book {
+// assert BookRequest as "any object"
+export interface BookRequest extends Record<string, unknown> {
   name: string;
   year: number;
   author: string;
@@ -7,4 +8,20 @@ export interface Book {
   pageCount: number;
   readPage: number;
   reading: boolean;
+}
+
+// assert BookStorage as "any object"
+export interface BookStorage extends Record<string, unknown> {
+  id: string;
+  name: string;
+  year: number;
+  author: string;
+  summary: string;
+  publisher: string;
+  pageCount: number;
+  readPage: number;
+  finished: boolean;
+  reading: boolean;
+  insertedAt: Date;
+  updatedAt: Date;
 }
