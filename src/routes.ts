bd2aef2c5551ad45e0fe0d2e033f9f904327efef
RyanOlder <68904054+RyanOld@users.noreply.github.com>
2023-09-17 08:54:41 +0700

    add API requirements for each routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,7 @@
 import type { Request, ResponseToolkit } from "@hapi/hapi";
 
 export const routes = [
-  {
-    method: "GET",
-    path: "/books",
-    handler: (request: Request, h: ResponseToolkit) => {
-      return "Hello World!";
-    },
-  },
+  // Kriteria 3 : API dapat menyimpan buku
   {
     method: "POST",
     path: "/books",
@@ -15,6 +9,15 @@ export const routes = [
       return "Book Added.";
     },
   },
+  // Kriteria 4 : API dapat menampilkan seluruh buku
+  {
+    method: "GET",
+    path: "/books",
+    handler: (request: Request, h: ResponseToolkit) => {
+      return "Hello World!";
+    },
+  },
+  // Kriteria 5 : API dapat menampilkan detail buku
   {
     method: "GET",
     path: "/books/{bookId}",
@@ -22,6 +25,7 @@ export const routes = [
       return "Book Library Returned.";
     },
   },
+  // Kriteria 6 : API dapat mengubah data buku
   {
     method: "PUT",
     path: "/books/{bookId}",
@@ -29,6 +33,7 @@ export const routes = [
       return "Book Data Modified.";
     },
   },
+  // Kriteria 7 : API dapat menghapus buku
   {
     method: "DELETE",
     path: "/books/{bookId}",
